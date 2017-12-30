7ee50779d217e2685ccde73b1ab9a3cc83944d00
Mohamed Aly <alydemah@gmail.com>
2017-12-30 12:00:36 +0100

    Hello World Component

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import React from 'react';
+
+class App extends React.Component {
+  render(){
+    return <h1>Hello World</h1>
+  }
+}
+
+export default App
+
+
+/*
+Different ways to Create React Components and when to use each.
+
+
+
+
+Container Components
+
+
+Presentation Components
+
+
+
+*/
