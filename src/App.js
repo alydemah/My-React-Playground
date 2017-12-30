1a252b361bacb96d6500153a83211b71530d69bf
Mohamed Aly <alydemah@gmail.com>
2017-12-30 13:52:12 +0100

    setting and updating state

--- src/App.js
+++ src/App.js
@@ -3,13 +3,31 @@ import React from 'react';
 // Class Component
 // Can have state
 class App extends React.Component {
+  constructor(){
+  super();
+  this.state = {
+    title: "this is state value"
+  }
+  }
+
+  update(event){
+    this.setState({title: event.target.value})
+  }
+
   render(){
     //let title = this.props.title;
     return (
-           <h1>{this.props.title}</h1>
+      <div>
+          <input type="text" 
+          onChange={this.update.bind(this)}/>
+
+            <h1>{this.state.title}</h1>
+        <h1>{this.props.title}</h1>
 
            // OR
-           // <h1>{title}</h1>
+           {/* <h1>{title}</h1> */}
+
+      </div>     
     )
 }
 }
@@ -20,7 +38,7 @@ class App extends React.Component {
 
 App.defaultProps = {
  title: "This is default value and it can be override if we passed value to title"
- 
+
 }
 
 
