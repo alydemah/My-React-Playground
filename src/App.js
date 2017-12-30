c46a606a2514f56ec509019d94a6faad055c7c45
Mohamed Aly <alydemah@gmail.com>
2017-12-30 12:03:30 +0100

    statless vs statfull component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
 import React from 'react';
 
+// Class Component
+// Can have state
 class App extends React.Component {
   render(){
     return <h1>Hello World</h1>
   }
 }
+// This is Statless function Component
+// Can't have state
+// const App = () => <h1>Hello World</h1>
+
 
 export default App
 
