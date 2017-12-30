8472d12e3f440360f6074c910b17defcd764a565
Mohamed Aly <alydemah@gmail.com>
2017-12-30 17:42:12 +0100

    adding guestname, guestlist component

--- src/App.js
+++ src/App.js
@@ -1,7 +1,5 @@
 import React from 'react';
 
-// Class Component
-// Can have state
 class App extends React.Component {
   constructor(){
   super();
@@ -15,47 +13,22 @@ class App extends React.Component {
   }
 
   render(){
-    //let title = this.props.title;
     return (
       <div>
-          <input type="text" 
-          onChange={this.update.bind(this)}/>
-
             <h1>{this.state.title}</h1>
-        <h1>{this.props.title}</h1>
-
-           // OR
-           {/* <h1>{title}</h1> */}
-
+            <Button update={this.update.bind(this)}>
       </div>     
     )
 }
 }
-// This is Statless function Component
-// Can't have state
-// const App = () => <h1>Hello World</h1>
 
 
-App.defaultProps = {
- title: "This is default value and it can be override if we passed value to title"
 
-}
+const Button = (props) => 
+<input type="text" onChange={props.update}/>
+
+
 
 
 export default App
 
-
-/*
-Different ways to Create React Components and when to use each.
-
-
-
-
-Container Components
-
-
-Presentation Components
-
-
-
-*/
